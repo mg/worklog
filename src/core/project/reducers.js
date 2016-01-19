72762accd850f8e994177b8e175a64ccd003d805
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2016-01-19 00:57:15 +0000

    project reducers: edit, saving, saved

--- src/core/project/reducers.js
+++ src/core/project/reducers.js
@@ -15,17 +15,48 @@ const initialState= Map({
 })
 
 const initialize= (state) => {
-  return state.set('project', Map({}))
+  return state.set(
+    'new', Map({})
+  )
 }
 
 const creating= (state, name) => {
-  return state.updateIn(['project'], project => project.set('name', name))
+  return state.updateIn(
+    ['new'], project => project.set('name', name)
+  )
 }
 
 const created= (state, project) => {
   return state
-    .updateIn(['projects'], projects => projects.push(Map(project)))
-    .delete('project')
+    .updateIn(
+      ['projects'], projects => projects.push(Map(project))
+    )
+    .delete('new')
+}
+
+const edit= (state, project) => {
+  return state.set(
+    'edit', Map({
+      index: state.get('projects').indexOf(project),
+      project: project,
+      saving: false,
+    })
+  )
+}
+
+const saving= (state) => {
+  return state.updateIn(
+    ['edit'], edit => edit.set('saving', true)
+  )
+}
+
+const saved= (state) => {
+  return state
+    .updateIn(
+      ['projects', state.get('edit').get('index')],
+      project => state.get('edit').get('project')
+    )
+    .delete('edit')
 }
 
 const reducer= (state= initialState, action) => {
@@ -36,9 +67,15 @@ const reducer= (state= initialState, action) => {
     return creating(state, action.payload)
   case 'CREATED_PROJECT':
     return created(state, action.payload)
+  case 'EDIT_PROJECT':
+    return edit(state, action.payload)
+  case 'SAVING_PROJECT':
+    return saving(state)
+  case 'SAVED_PROJECT':
+    return saved(state)
   }
   return state;
 }
 
 export default reducer
-export { initialize, creating, created }
+export { initialize, creating, created, edit, saving, saved }
--- src/core/project/reducers.spec.js
+++ src/core/project/reducers.spec.js
@@ -1,27 +1,51 @@
 import { expect } from 'chai'
 import { Map, fromJS } from 'immutable'
 
-import { initialize, creating, created } from './reducers.js'
+import { initialize, creating, created, edit, saving, saved } from './reducers.js'
 
 describe('project/reducers ->', () => {
   it('initializes an empty project', () => {
     const before= fromJS({})
-    const after= fromJS({project: {}})
+    const after= fromJS({new: {}})
 
     expect(initialize(before)).to.equal(after)
   })
 
   it('stores name when creating', () => {
-    const before= fromJS({project: {}})
-    const after= fromJS({project: {name: 'NAME'}})
+    const before= fromJS({new: {}})
+    const after= fromJS({new: {name: 'NAME'}})
 
     expect(creating(before, 'NAME')).to.equal(after)
   })
 
   it('stores new project after it is created, deletes project placeholder', () => {
-    const before= fromJS({projects: [], project: {}})
+    const before= fromJS({projects: [], new: {}})
     const after= fromJS({projects: [{name: 'NAME'}]})
 
     expect(created(before, {name: 'NAME'})).to.equal(after)
+  })
+
+  it('sets state up for editing a project', () => {
+    const project= fromJS({name: 'NAME'})
+    const before= fromJS({projects: [project]})
+    const after= fromJS({projects: [project], edit: {index: 0, project: project, saving: false}})
+
+    expect(edit(before, project)).to.equal(after)
+  })
+
+  it('marks project as being saved', () => {
+    const project= fromJS({name: 'NAME'})
+    const before= fromJS({projects: [project], edit: {index: 0, project: project, saving: false}})
+    const after= fromJS({projects: [project], edit: {index: 0, project: project, saving: true}})
+
+    expect(saving(before)).to.equal(after)
+  })
+
+  it('saves changes', () => {
+    const project= fromJS({name: 'NEWNAME'})
+    const before= fromJS({projects: [{name: 'NAME'}], edit: {index: 0, project: project}})
+    const after= fromJS({projects: [project]})
+
+    expect(saved(before)).to.equal(after)
   })
 })
