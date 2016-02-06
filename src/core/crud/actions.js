c717c35f04507377974b2fd77508013a7fe16e16
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2016-02-06 10:19:39 +0000

    crud reducers: error actions

--- src/core/crud/actions.js
+++ src/core/crud/actions.js
@@ -1,8 +1,9 @@
 export default function(name) {
-  const action= (prefix, payload) => {
+  const action= (prefix, payload, key) => {
     return {
       type: `${prefix}_${name}`,
       payload,
+      key,
     }
   }
 
@@ -14,15 +15,27 @@ export default function(name) {
 
   const loadItems= (payload) => action('LOAD_ITEMS', payload)
   const create= (payload) => action('CREATE', payload)
-  const creating= (payload) => action('CREATING', payload)
-  const created= (payload) => action('CREATED', payload)
+  const creating= (payload, key) => action('CREATING', payload, key)
+  const created= (payload, key) => action('CREATED', payload, key)
   const edit= (payload) => action('EDIT', payload)
-  const save= (payload) => action('SAVE', payload)
-  const saving= (payload) => action('SAVING', payload)
+  const save= (payload, key) => action('SAVE', payload, key)
+  const saving= (payload, key) => action('SAVING', payload, key)
   const saved= (payload) => action('SAVED', payload)
   const remove= (payload) => action('REMOVE', payload)
-  const removing= (payload) => action('REMOVING', payload)
+  const removing= (payload, key) => action('REMOVING', payload, key)
   const removed= (payload) => action('REMOVED', payload)
+
+  const error= (action, description) => {
+    return {
+      ...action,
+      isError: true,
+      description,
+    }
+  }
+
+  const errorCreating= (description, key) => error(created({}, key), description)
+  const errorSaving= (description, key) => error(saved({}, key), description)
+  const errorRemoving= (description, key) => error(removed({}, key), description)
 
   return {
     loadItems, initialize,
@@ -30,5 +43,6 @@ export default function(name) {
     edit,
     save, saving, saved,
     remove, removing, removed,
+    errorCreating, errorSaving, errorRemoving,
   }
 }
