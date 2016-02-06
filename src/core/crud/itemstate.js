f02f511a37038cba900b43185daab5bcb1f1e568
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2016-02-06 10:20:32 +0000

    crud reducers: error item states

--- src/core/crud/itemstate.js
+++ src/core/crud/itemstate.js
@@ -4,13 +4,13 @@ export function setInitializing(obj, init) {
 }
 
 export function setEditing(obj, editing) {
-  if(editing) return obj.set('isEditing', true)
+  if(editing) return setError(obj, false).set('isEditing', true)
   return obj.delete('isEditing')
 }
 
 export function setCreating(obj, creating) {
   if(creating) {
-    return obj
+    return setError(obj, false)
       .set('isCreating', true)
       .delete('isInitializing')
   }
@@ -18,11 +18,16 @@ export function setCreating(obj, creating) {
 }
 
 export function setSaving(obj, saving) {
-  if(saving) return obj.set('isSaving', true)
+  if(saving) return setError(obj, false).set('isSaving', true)
   return obj.delete('isSaving')
 }
 
 export function setRemoving(obj, removing) {
-  if(removing) return obj.set('isRemoving', true)
+  if(removing) return setError(obj, false).set('isRemoving', true)
   return obj.delete('isRemoving')
+}
+
+export function setError(obj, error, description) {
+  if(error) return obj.set('isError', true).set('error', description)
+  return obj.delete('isError').delete('error')
 }
