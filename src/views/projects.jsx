a5ea9acfb67ce259269054b2efb1dec90e946177
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2016-02-09 23:06:09 +0000

    views: projects

--- src/views/projects.jsx
+++ src/views/projects.jsx
@@ -1,11 +1,44 @@
 import React from 'react'
+import Radium from 'radium'
+
+import List from 'widgets/crud/list'
 
 export class Projects extends React.Component {
   render() {
+    const { projects }= this.props
+
     return (
-      <div>Projects</div>
+      <div>
+        <h4>Projects</h4>
+        <List
+          items={projects}
+          onSelect={item => console.log('select:', item.get('name'))}
+          onEdit={item => console.log('edit:', item.get('name'))}
+          onRemove={item => console.log('remove:', item.get('name'))}
+          onCreate={() => console.log('create')}
+          >
+          {project => {
+            return (
+              <div>
+                {project.get('name')}
+              </div>
+            )
+          }}
+        </List>
+      </div>
     )
   }
 }
 
-export default Projects
+// connect actions and state
+import { connect } from 'react-redux'
+export default connect(
+  state => {
+    return {
+      projects: state.projects
+    }
+  },
+  {
+    // actions
+  }
+)(Radium(Projects))
