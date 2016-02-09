ccb9cdeec054517e4329bafd1c0a02b4d41a701c
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2016-02-09 23:04:21 +0000

    crud widgets: actions

--- src/widgets/crud/item.jsx
+++ src/widgets/crud/item.jsx
@@ -4,26 +4,41 @@ import PropTypes from 'react-immutable-proptypes'
 
 import { getItem } from 'store/crud/access'
 
+import Button from './button.jsx'
+
 export class Item extends React.Component {
   static propTypes= {
     item: PropTypes.map.isRequired,
     state: PropTypes.map,
     style: React.PropTypes.object,
+    onSelect: React.PropTypes.func,
+    onEdit: React.PropTypes.func,
+    onRemove: React.PropTypes.func,
   }
 
   render() {
-    const { item, state, style, children }= this.props
+    const { item, state, style, onEdit, onRemove, children }= this.props
 
     let itemStyle= styles.item
     if(style !== undefined) {
       itemStyle= {...itemStyle, ...style}
     }
-    
+
     return (
       <li style={itemStyle}>
-        {children(item)}
+        <a href='#' onClick={::this.onClick} style={styles.link}>
+          {children(item)}
+        </a>
+        <Button onClick={() => onRemove(item)}>clear</Button>
+        <Button onClick={() => onEdit(item)}>create</Button>
       </li>
     )
+  }
+
+  onClick(e) {
+    const { item, onSelect }= this.props
+    e.preventDefault()
+    onSelect(item)
   }
 }
 
@@ -32,5 +47,23 @@ export default Radium(Item)
 const styles= {
   item: {
     display: 'flex',
+    padding: 6,
+
+    ':hover': {
+      backgroundColor: '#eee',
+    }
+  },
+
+  link: {
+    textDecoration: 'none',
+    color: 'black',
+    marginRight: 4,
   }
 }
+
+// state icons
+// retry: cached
+// done: done
+// remove: clear
+// edit: create
+// new: add
--- src/widgets/crud/list.jsx
+++ src/widgets/crud/list.jsx
@@ -6,6 +6,7 @@ import {
   getItem, isInitializing, isEditing, isCreating, isSaving, isRemoving,
 } from 'store/crud/access'
 
+import Button from './button.jsx'
 import Item from './item.jsx'
 
 export class List extends React.Component {
@@ -13,10 +14,15 @@ export class List extends React.Component {
     items: PropTypes.map,
     style: React.PropTypes.object,
     itemStyle: React.PropTypes.object,
+
+    onSelect: React.PropTypes.func,
+    onCreate: React.PropTypes.func,
+    onEdit: React.PropTypes.func,
+    onRemove: React.PropTypes.func,
   }
 
   render() {
-    const { items, style, itemStyle, children }= this.props
+    const { items, style, itemStyle, onSelect, onCreate, onEdit, onRemove, children }= this.props
 
     if(items === undefined) {
       return null
@@ -28,19 +34,29 @@ export class List extends React.Component {
     }
 
     return (
-      <ul style={listStyle}>
-        {items.map(
-          (item, key) =>
-            <Item
-              key={key}
-              item={getItem(item)}
-              state={item}
-              style={itemStyle}
-              >
-              {children}
-            </Item>
-        ).toArray()}
-      </ul>
+      <div style={styles.container}>
+        <ul style={listStyle}>
+          {items.map(
+            (item, key) =>
+              <Item
+                key={key}
+                item={getItem(item)}
+                state={item}
+                style={itemStyle}
+                onSelect={onSelect}
+                onEdit={onEdit}
+                onRemove={onRemove}
+                >
+                {children}
+              </Item>
+          ).toArray()}
+        </ul>
+        <div style={styles.buttonPanel}>
+          <Button style={styles.button} onClick={onCreate}>
+            add
+          </Button>
+        </div>
+      </div>
     )
   }
 }
@@ -48,8 +64,22 @@ export class List extends React.Component {
 export default Radium(List)
 
 const styles= {
+  container: {
+    display: 'inline-block'
+  },
+
   list: {
     margin: 0,
     padding: 0,
+    display: 'inline-block'
+  },
+
+  buttonPanel: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+
+  button: {
+    padding: '5px 40px',
   }
 }
