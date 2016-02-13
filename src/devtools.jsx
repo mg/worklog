1b3379c2a347b8eb100e8e083f5e101ee9c99b45
Magnús Örn Gylfason <magnus.gylfason@gmail.com>
2016-02-13 19:26:09 +0000

    redux devtools: dispatch

--- src/devtools.jsx
+++ src/devtools.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import { createDevTools } from 'redux-devtools'
 import LogMonitor from 'redux-devtools-log-monitor'
 import DockMonitor from 'redux-devtools-dock-monitor'
+import Dispatcher from 'redux-devtools-dispatch'
+import MultipleMonitors from 'redux-devtools-dispatch/lib/MultipleMonitors'
 
 export default createDevTools(
-  <DockMonitor toggleVisibilityKey='ctrl-h' changePositionKey='ctrl-q'>
-    <LogMonitor />
+  <DockMonitor toggleVisibilityKey='ctrl-h' changePositionKey='ctrl-q' defaultIsVisible={false}>
+    <MultipleMonitors>
+      <LogMonitor />
+      <Dispatcher/>
+    </MultipleMonitors>
   </DockMonitor>
 )
